9a1d2b216558b2ef4d5add00f4cbbe1084e2a2ff
yanchangyou <yanchangyou@gmail.com>
2016-10-29 21:37:01 +0800

    对象不使用斜杠分割，文件之间加上换行符

--- git/cat_git_objects.sh
+++ git/cat_git_objects.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # 放到 .git目录下面
 cd objects
-find . -type f|grep -E '^./\w+'|awk -F / '{print "echo =======objects : "$2"/"$3"===== && git cat-file -p "$2$3}'|xargs -0  bash -c
+find . -type f|grep -E '^./\w+'|awk -F / '{print "echo =======objects : "$2$3"===== && git cat-file -p "$2$3" &&echo"}'|xargs -0  bash -c
